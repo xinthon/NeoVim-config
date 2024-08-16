27a632b340ab7da209cb33159c0ce0773278cda3
Sinthon Seng <s.sinthon@cas.com.kh>
2024-08-16 12:56:17 +0700

    init

---
+++ lua/chadrc.lua
@@ -0,0 +1,37 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "chocolate",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.ui = {
+  transparency = true,
+  nvdash = 
+  {
+    load_on_startup = true,
+    header = {
+        "Unleash your coding superpowers! ✨",
+        "Make each line of code your masterpiece 🎨",
+    },
+    buttons = 
+    {
+        { "  Find File", "Spc f f", "Telescope find_files" },
+        { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+        { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+        { "  Bookmarks", "Spc m a", "Telescope marks" },
+        { "  Themes", "Spc t h", "Telescope themes" },
+        { "  Mappings", "Spc c h", "NvCheatsheet" },
+    }
+  }  
+}
+
+return M
